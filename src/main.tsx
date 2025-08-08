9abcfaf20219c230dc48884abb105968deefca51
Truong Van Thanh <truong.van.thanh@vareal.vn>
2025-08-08 17:09:07 +0700

    Add ant design icon, scss, completed design login page

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { store } from './redux/store'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 
+import './index.css'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
